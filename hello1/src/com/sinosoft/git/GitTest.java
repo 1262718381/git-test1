106f70a5419c562bd60e3a0afae62de9665bf96f
liulinjun <1262718381@qq.com>
2021-04-24 16:37:18 +0800

    把本地工程 添加到给git

---
+++ hello1/src/com/sinosoft/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.sinosoft.git;
+
+/**
+ * @Author: liulinjun
+ * @Date: 2021/4/24 16:31
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println(11);
+    }
+}
